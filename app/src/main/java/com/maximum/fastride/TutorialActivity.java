190ce94708e963afeaf6b7b3731fe1c2d7c019a2
olegkleiman <oleg_kleyman@hotmail.com>
2015-05-16 13:18:07 +0300

    Alpha 0.44 <= Initial Tutorials Activity

--- app/src/main/java/com/maximum/fastride/TutorialActivity.java
+++ app/src/main/java/com/maximum/fastride/TutorialActivity.java
@@ -1,7 +1,7 @@
 package com.maximum.fastride;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBar;
-import android.app.FragmentTransaction;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -9,17 +9,21 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
-import com.maximum.fastride.R;
-import com.maximum.fastride.adapters.ImageAdapter;
+import com.maximum.fastride.adapters.TutorialTabsAdapter;
+import com.maximum.fastride.tutorials.DriverTutorialFragment;
+import com.maximum.fastride.tutorials.IntroTutorialFragment;
+import com.maximum.fastride.tutorials.PassengerTutorialFragment;
+import com.maximum.fastride.tutorials.ReclaimTutorialFragment;
+import com.maximum.fastride.views.SlidingTabLayout;
 
 public class TutorialActivity extends ActionBarActivity
         implements ActionBar.TabListener{
 
-    AppSectionsPagerAdapter mAppSectionsPagerAdapter;
+    TutorialTabsAdapter mAppSectionsPagerAdapter;
     ViewPager mViewPager;
+    SlidingTabLayout slidingTabLayout;
+    private String titles[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,61 +33,23 @@ public class TutorialActivity extends ActionBarActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.fastride_toolbar);
         setSupportActionBar(toolbar);
 
-//        ViewFlow viewFlow = (ViewFlow) findViewById(R.id.viewflow);
-//        viewFlow.setAdapter(new ImageAdapter(this), 5);
-//        CircleFlowIndicator indic = (CircleFlowIndicator) findViewById(R.id.viewflowindic);
-//        viewFlow.setFlowIndicator(indic);
+        titles = getResources().getStringArray(R.array.tutorial_titles);
 
         // Set up the ViewPager, attaching the adapter and setting up a listener for when the
         // user swipes between sections.
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        // Create the adapter that will return a fragment for each of the three primary sections
-        // of the app.
-        mAppSectionsPagerAdapter = new AppSectionsPagerAdapter(getSupportFragmentManager());
-//        mViewPager.setAdapter(mAppSectionsPagerAdapter);
-//        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-//            @Override
-//            public void onPageSelected(int position) {
-////                 When swiping between different app sections, select the corresponding tab.
-////                 We can also use ActionBar.Tab#select() to do this if we have a reference to the
-////                 Tab.
-////                toolbar.setSelectedNavigationItem(position);
-//            }
-//        });
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        slidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
+        mViewPager.setAdapter(new TutorialTabsAdapter(getSupportFragmentManager(),
+                                                        titles));
 
-        // For each of the sections in the app, add a tab to the action bar.
-//        for (int i = 0; i < mAppSectionsPagerAdapter.getCount(); i++) {
-//            // Create a tab with text corresponding to the page title defined by the adapter.
-//            // Also specify this Activity object, which implements the TabListener interface, as the
-//            // listener for when this tab is selected.
-//            toolbar.addTab(
-//                    actionBar.newTab()
-//                            .setText(mAppSectionsPagerAdapter.getPageTitle(i))
-//                            .setTabListener(this));
-//        }
-    }
+        slidingTabLayout.setViewPager(mViewPager);
+        slidingTabLayout.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return Color.WHITE;
+            }
+        });
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_tutorial, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -102,25 +68,4 @@ public class TutorialActivity extends ActionBarActivity
     }
 
 
-    public static class AppSectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public AppSectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return null;
-        }
-
-        @Override
-        public int getCount() {
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return "Section " + (position + 1);
-        }
-    }
 }
