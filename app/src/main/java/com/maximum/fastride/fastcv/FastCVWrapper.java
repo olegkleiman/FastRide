38007ade82a8c3e006628e69158b4ee880b939db
Oleg Kleiman <oleg_kleyman@hotmail.com>
2015-08-17 03:27:44 +0300

    1.3.2 - using cascade in face detection

--- app/src/main/java/com/maximum/fastride/fastcv/FastCVWrapper.java
+++ app/src/main/java/com/maximum/fastride/fastcv/FastCVWrapper.java
@@ -1,5 +1,7 @@
 package com.maximum.fastride.fastcv;
 
+import org.opencv.core.MatOfRect;
+
 /**
  * Created by Oleg Kleiman on 05-Jul-15.
  */
@@ -9,14 +11,16 @@ public class FastCVWrapper {
 //        System.loadLibrary("fastcvUtils");
 //    }
 
-    public FastCVWrapper(){
+    public String pathToCascade;
 
+    public FastCVWrapper(String path){
+        pathToCascade = path;
     }
 
     public native void FrameTick();
     public native void FindFeaturesFAST(long matAddrGr, long matAddrRgba);
     public native void FindFeaturesORB(long matAddrGr, long matAddrRgba);
     public native void FindFeaturesKAZE(long matAddrGr, long matAddrRgba);
-    public native void DetectFaces(long matAddrRgba, String face_cascade_name);
+    public native int DetectFaces(long matAddrRgba, String face_cascade_name);
     public native void Blur(long matAddrGr);
 }
